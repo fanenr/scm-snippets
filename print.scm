7e68a6704cecbccbea5ee9141413d9834538de91
Arthur63 <Arthur63@163.com>
2024-04-27 19:24:22 +0800

    update print.scm

--- print.scm
+++ print.scm
@@ -1,25 +1,14 @@
-#!/usr/bin/guile3.0 -s
-!#
+(define (print-list args)
+  (unless (null? args)
+    (display (car args))
+    (for-each (lambda (arg) (display #\space) (display arg)) (cdr args))))
 
 (define (print . args)
-  (for-each display args))
+  (print-list args))
 
 (define (println . args)
-  (for-each display args)
+  (print-list args)
   (newline))
-
-(define str1 "hello")
-(define str2 "hello")
-
-(print "(eqv? str1 str2): ")
-(println (eqv? str1 str2))
-
-(newline)
-
-(print "(equal? str1 str2): ")
-(println (equal? str1 str2))
-
-(newline)
 
 (define (my-for-each pro ls)
   (when (and (list? ls) (not (null? ls)))
@@ -29,21 +18,22 @@
       (unless (null? rest)
         (my-for-each pro rest)))))
 
-(define (my-print-impl args)
-  (when (and (list? args) (not (null? args)))
-    (let ((first (car args)) (rest (cdr args)))
-      (unless (null? first)
-        (display first))
-      (unless (null? rest)
-        (display #\space)
-        (my-print-impl rest)))))
+(define (my-print-list args)
+  (unless (null? args)
+    (display (car args))
+    (my-for-each (lambda (arg) (display #\space) (display arg)) (cdr args))))
 
 (define (my-print . args)
-  (my-for-each (lambda (arg) (display arg) (display #\space)) args))
+  (my-print-list args))
 
 (define (my-println . args)
-  (my-print args)
+  (my-print-list args)
   (newline))
+
+(println)
+(println 1)
+(println 1 2)
+(println 1 2 3)
 
 (my-println)
 (my-println 1)
