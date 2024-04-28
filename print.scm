bb8191d5b84fa3a4dc257c0f02ce84ef83faa566
Arthur63 <arthur63@163.com>
2024-04-28 20:25:35 +0800

    try do and let

--- print.scm
+++ print.scm
@@ -1,10 +1,12 @@
 (define (print-list args)
-  (unless (null? args)
-    (display (car args))
-    (for-each (lambda (arg) 
-                (display #\space)
-                (display arg))
-              (cdr args))))
+  (when (and (list? args) (not (null? args)))
+    (let ((first (car args)) (rest (cdr args)))
+      (unless (null? first)
+        (display first))
+      (for-each (lambda (arg)
+                  (display #\space)
+                  (display arg))
+                rest))))
 
 ; (define print (lambda args
 ;                 (print-list args)))
@@ -12,36 +14,51 @@
 (define (print . args)
   (print-list args))
 
-; (define println (lambda args
-;                   (print-list args)
-;                   (newline)))
-
-(define (println . args)
+(define (println-list args)
   (print-list args)
   (newline))
 
-(define (my-for-each pro ls)
+; (define println (lambda args
+;                   (println-list args)))
+
+(define (println . args)
+  (println-list args))
+
+(define (my-for-each proc ls)
   (when (and (list? ls) (not (null? ls)))
     (let ((first (car ls)) (rest (cdr ls)))
       (unless (null? first)
-        (pro first))
+        (proc first))
       (unless (null? rest)
-        (my-for-each pro rest)))))
+        (my-for-each proc rest)))))
 
 (define (my-print-list args)
-  (unless (null? args)
-    (display (car args))
-    (my-for-each (lambda (arg)
-                   (display #\space)
-                   (display arg))
-                 (cdr args))))
+  (when (and (list? args) (not (null? args)))
+    (let ((first (car args)) (rest (cdr args)))
+      (unless (null? first)
+        (display first))
+      (my-for-each (lambda (arg)
+                     (display #\space)
+                     (display arg))
+                   rest))))
+
+; (define my-print (lambda args
+;                    (my-print-list args)))
 
 (define (my-print . args)
   (my-print-list args))
 
-(define (my-println . args)
+(define (my-println-list . args)
   (my-print-list args)
   (newline))
+
+; (define my-println (lambda args
+;                      (my-println-list args)))
+
+(define (my-println . args)
+  (my-println-list args))
+
+;;; examples
 
 ; (println)
 ; (println 1)
