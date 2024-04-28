c3408b967ea2cc034c2ec364ee9b5d84250f901d
Arthur63 <Arthur63@163.com>
2024-04-28 20:25:35 +0800

    try do and let

--- main.scm
+++ main.scm
@@ -7,19 +7,32 @@
 (define (++ n)
   (+ n 1))
 
-(define (assert bool msg)
-  (unless bool
-    (println msg)
+; (define (assert expr . strs)
+;   (unless expr
+;     (call-with-values (lambda () (apply values strs))
+;                       println)
+;     (exit 1)))
+
+(define (assert expr . strs)
+  (unless expr
+    (println-list strs)
     (exit 1)))
 
 (define (main args)
   (define args-size (length args))
-  (assert (>= args-size 1) "Please pass a number")
+  ; (unless (eqv? args-size 2)
+  ;   (error "usage:" (car args) "<max>"))
+  (assert (eqv? args-size 2) "usage:" (car args) "<max>")
 
   (define max (string->number (list-ref args 1)))
-  (assert max "Please pass a number")
+  ; (unless (integer? max)
+  ;   (error "Please pass a integer"))
+  (assert (integer? max) "please pass a integer")
 
-  (let loop ((i 0))
-    (println i)
-    (if (>= i max) 'null
-      (loop (++ i)))))
+  ; (let loop ((i 0))
+  ;   (println i)
+  ;   (if (>= i max) 'null
+  ;     (loop (++ i))))
+
+  (do ((i 0 (++ i))) ((>= i max) 'null)
+    (println i)))
