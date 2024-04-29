605614b208f9ac4020b8590db8d0369972f48d37
Arthur63 <Arthur63@163.com>
2024-04-29 22:17:23 +0800

    try io and module

---
+++ io.scm
@@ -0,0 +1,23 @@
+#!/usr/bin/guile3.0 \
+-e main -s
+!#
+
+(use-modules (ice-9 rdelim) (ice-9 binary-ports) (ice-9 textual-ports))
+
+(define stdin (current-input-port))
+(define stdout (current-output-port))
+
+(define (main . args)
+  (define file "a.txt")
+  (unless (file-exists? file)
+    (close-port (open-file file "w")))
+
+  (define port (open-file "a.txt" "w"))
+
+  (let loop ()
+    (define ch (read-char))
+    (unless (eof-object? ch)
+      (put-char port ch)
+      (loop)))
+
+  (close-port port))
