5942be84b7c64c100e6836efc249addb42c1caa2
Arthur63 <arthur63@163.com>
2024-05-04 22:36:08 +0800

    update io.scm

--- iter.scm
+++ iter.scm
@@ -1,21 +1,22 @@
 (use-modules (ice-9 format))
 
-(let ()
+(let ((max 10))
   (define (loop i)
     (when (< i max)
       (format #t "~a " i)
       (loop (+ i 1))))
-  (define max 10)
-  (loop 0)
-  (newline))
+  (loop 0))
+
+(newline)
 
 (letrec ((loop (lambda (i)
                  (when (< i max)
                    (format #t "~a " i)
                    (loop (+ i 1)))))
          (max 10))
-  (loop 0)
-  (newline))
+  (loop 0))
+
+(newline)
 
 ; (do ((max 10 10) (i 0 (+ i 1)))
 ;     ((>= i max) 'nothing)
@@ -25,17 +26,20 @@
 (do ((max 10) (i 0 (+ i 1)))
     ((>= i max))
   (format #t "~a " i))
+
 (newline)
 
 (let ((max 10) (i 0))
   (while (< i max)
     (format #t "~a " i)
-    (set! i (+ i 1)))
-  (newline))
+    (set! i (+ i 1))))
+
+(newline)
 
 (let ((max 10))
   (let loop ((i 0))
     (when (< i max)
       (format #t "~a " i)
-      (loop (+ i 1))))
-  (newline))
+      (loop (+ i 1)))))
+
+(newline)
